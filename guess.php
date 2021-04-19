a0b5d96f0d5c71c2594bba9dd91ae1e0af88c7b7
shaza955 <80900503+shaza955@users.noreply.github.com>
2021-04-19 21:53:31 +0300

    Add files via upload

---
+++ guess.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Guessing Game for Charles Severance 10220ded </title>
+</head>
+<body>
+<h1> Welcome to my guessing game </h1>
+<p>Your guess is too low </p>
+<?php 
+$correctnumber = 18;
+if(isset($_GET['guess'])){
+	if(is_numeric($_GET['guess']) === FALSE){
+		echo "Your guess is not a number";
+	}
+	else if ($_GET['guess']<$correctnumber) {
+		echo "Your guess is too low ";
+	}
+	else if ($_GET['guess']>$correctnumber) {
+		echo "Your guess is too high ";
+	}
+	else if ($_GET['guess']==$correctnumber) {
+		echo "Congratulations - You are right";
+	}
+}
+else{
+	echo "Missing guess parameter";
+	echo "Your is too short";
+}
+</body>
+</html>
