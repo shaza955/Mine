d623f7ed32a307c7f67b5677fcc0255c79ba1fe3
shaza955 <80900503+shaza955@users.noreply.github.com>
2021-04-22 21:12:03 +0300

    .

--- guess.php
+++ guess.php
@@ -1,10 +1,9 @@
 <!DOCTYPE html>
 <html>
-<head>
-	<title> Guessing game for Shaza Albodi</title>
-</head>
+<head></head>
 <body>
-<h1> Welcome to my guessing game </h1>
+<title> Shaza Albodi </title>
+<h1> Welcome to my guessing game</h1>
 <p>Your guess is too low </p>
 <?php 
 $correctnumber = 18;
